98f0def7b8a8d33deb7e5c70dcc28499eec93dde
Aniket <aniketdevmore1008@gmail.com>
2023-09-17 16:08:31 +0530

    install typescript

---
+++ with-typescript.js
@@ -0,0 +1,8 @@
+function addnum(a, b) {
+  return a + b;
+}
+console.log(addnum(2, 3));
+//typescript is superset to javascript
+// npm install typescript
+//does not run in browser
+//npx tsc with-typescript.ts
---
+++ with-typescript.ts
@@ -0,0 +1,10 @@
+//typescript is superset to javascript
+
+// npm install typescript
+//does not run in browser
+//npx tsc with-typescript.ts
+
+function addnum1(a: number, b: number) {
+  return a + b;
+}
+console.log(addnum1(2, 3));
